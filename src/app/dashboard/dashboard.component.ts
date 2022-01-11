91ed8cc1871ebcb733c785c3b509596e52a43098
MeghanaHP <meghaanahp@gmail.com>
2022-01-11 13:14:55 +0530

    updated environment.prod.ts

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Path } from '../models';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+    this.router.navigateByUrl(Path.PRODUCTS);
   }
 
 }
--- src/app/dashboard/header/header.component.ts
+++ src/app/dashboard/header/header.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router, RouterEvent } from '@angular/router';
 import { AuthService } from 'src/app/security/auth.service';
 import { Path } from 'src/app/models/constant';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -14,12 +15,14 @@ currentUrl;
   constructor(
     private authService: AuthService,
     private router: Router,
-    private activatedRoute: ActivatedRoute) { }
+    private activatedRoute: ActivatedRoute) {
+      this.router.events.pipe(filter(ev => ev instanceof NavigationEnd)). subscribe((ev => {
+        this.currentUrl = ev['url'].replace('/', '');
+       }));
+     }
 
   ngOnInit(): void {
-    this.activatedRoute.url.subscribe(url => {
-        this.currentUrl = url;
-    })
+    
     this.authService.getSessionUser();
   }
 
